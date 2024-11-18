3e218699c0e2ec39552490ff3c6950aa9663801a
unknown <dernimohammed4@gamil.com>
2024-11-18 13:53:02 +0100

    add user and product

--- db.js
+++ db.js
@@ -1,15 +1,16 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient,ObjectId } = require("mongodb");
 const user=require('./models/user');
-
-
 // Connexion à MongoDB Atlas
 const uri =
-  "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.3.3";
+  "mongodb://127.0.0.1:27017";
 
 const client = new MongoClient(uri);
 const store = "store"; // database name
 const users = "users"; // users collection name
 const products = "products"; // products collection name
+const panier="panier";
+
+
 
 //get user function
 async function getuser(username, password) {
@@ -18,7 +19,7 @@ async function getuser(username, password) {
     const dbb = client.db(store);
     const collection = dbb.collection(users);
     const fetch_users = await collection
-      .find({ name: username, pasword: password })
+      .find({ name: username, age: password })
       .toArray();
       console.log(fetch_users)
     return fetch_users;
@@ -44,18 +45,39 @@ async function getProduct() {
   }
 }
 
-
 //post user
 async function adduser(namein,emailin,passwordin) {
   try {
     await client.connect();
     const dbb = client.db(store);
     const collection = dbb.collection(users);
-    const newUser = new User({
+    const newUser = {
       username: namein,
       email: emailin,
       password:passwordin
-    });
+    };
+    const add_users = await collection.insertOne(newUser);
+    return add_users;
+  } catch (error) {
+    console.log(error);    
+  }
+  finally {
+    await client.close();
+  }   
+}
+
+//post product
+
+async function addproduct(name,category,img) { 
+  try {
+    await client.connect();
+    const dbb = client.db(store);
+    const collection = dbb.collection(products);
+    const newProduct = {
+      pname: name,
+      pcategory: category,
+      password:passwordin
+    };
     const add_users = await collection.insertOne(newUser);
     return add_users;
   } catch (error) {
@@ -66,5 +88,42 @@ async function adduser(namein,emailin,passwordin) {
   }
 }
 
+async function addPanier(user,product) {
+  try {
+    await client.connect();
+    const dbb = client.db(store);
+    const collection = dbb.collection(panier);
+    const newPanier = {
+      iduser: user,
+      idproduct: product,
+      commande:0
+    };
+    const add_panier = await collection.insertOne(newPanier);
+    return add_panier;
+  } catch (error) {
+    console.log(error);    
+  }
+  finally {
+    await client.close();
+  }
+}
 
-module.exports = { getuser, getProduct,adduser,client};
+async function updatecommand(id,status) {
+  try {
+    await client.connect();
+    const dbb = client.db(store);
+    const collection = dbb.collection(panier);
+    const updatedPanier = await collection.findByIdAndUpdate(id, { $set: status }, { new: true });
+    return updatedPanier;
+  } catch (error) {
+    console.log(error);
+  }
+  finally {
+    await client.close();
+  }
+
+
+}
+
+
+module.exports = { getuser, getProduct,adduser,addproduct,addPanier,updatecommand};
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getuser, getProduct, adduser,client } = require("./db");
+const { getuser, getProduct, adduser,addproduct,addPanier,updatecommand} = require("./db");
 const { showdata } = require('./functions')
 const Produit = require('./models/product');
 
@@ -23,7 +23,7 @@ app.get("/", (req, res) => {
   });
 // authentification
 app.get("/get-user", async (req, res) => {
-  const user_result = await getuser(user_test.username, user_test.password);
+  const user_result = await getuser('mohammed',25);
   showdata(user_result, res)
 });
 
@@ -33,10 +33,11 @@ app.get("/get-products", async (req, res) => {
   showdata(product_result, res);
 });
 
-  app.post('/user',async(req,res)=>{
+
+app.post('/user',async(req,res)=>{
     try
     {
-      const result = await adduser("salim","salim@gmail.com","salim123");
+      const result = await adduser(req.body.name,req.body.email,req.body.password);
       res.status(201).send({ message: 'Utilisateur ajouté avec succès', user: result });
       } catch (error) {
         console.error(error);
@@ -44,6 +45,58 @@ app.get("/get-products", async (req, res) => {
       }
     })
 
+    app.post('/produit', async (req, res) => {
+      try {
+        const result = await addproduct(req.body.name,req.body.category,req.body.img);
+        res.status(201).json({ message: 'produit ajouté avec succès', user: result });
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Erreur lors de l\'ajout du produit ' });
+      }
+      res.send('created')
+    })
+
+   app.post('/panier',async(req,res)=>{
+    try {
+      const  result =await addPanier(req.body.iduser,req.body.idproduit);
+      res.status(201).send({ message: 'produit ajouté au panier', user: result });
+    } catch (error) {
+      console.error(error);
+        res.status(500).send({ message: 'Erreur lors de l\'ajout du produit' });
+    }
+   })
+ app.put('/command/:id',async(req,res)=>
+ {
+  try {
+    const result=await updatecommand(req.params,req.body);
+
+    if(!result)
+    {
+      res.status(404).send({message:'command non trouve'});
+    }
+    else{
+      res.status(201).send({message:'command mise a jour'});
+    }    
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({message:'erreur lors de la mise a jour'})
+  }
+ }
+  
+)    
+/*
+let db, clientsCollection;
+
+MongoClient.connect(uri, { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connexion à MongoDB réussie');
+    db = client.db('store'); // Remplacez par le nom de votre base de données
+    clientsCollection = db.collection('user');
+  })
+  .catch(error => console.error(error));
+
+
+*/
 app.post('/commande',async(req,res)=>{
   try {
     await client.connect();
@@ -55,27 +108,8 @@ app.post('/commande',async(req,res)=>{
   }
 })
 
-app.post('/produit', async (req, res) => {
-  try {
-    // Créer un nouvel utilisateur avec les données de la requête
-    await client.connect();
-    const newproduct = new Produit({
-      name: req.body.name,
-      price: req.body.price,
-      category: req.body.category,
-      quantity: req.body.category
-    });
-    // Insérer l'utilisateur dans la collection MongoDB
-    const result = await newproduct.save();
-    res.status(201).json({ message: 'produit ajouté avec succès', user: result });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Erreur lors de l\'ajout de l\'utilisateur' });
-  }
-  res.send('created')
-})
 
-app.post('/user', async (req, res) => {
+/*app.post('/user', async (req, res) => {
   try {
     console.log("wait1..");
     const result = await adduser(req.body.username, req.body.email, req.body.password);
@@ -87,7 +121,7 @@ app.post('/user', async (req, res) => {
   }
   res.send('created')
 });
-
+*/
   app.listen(port, () => {
     console.log(`server is running on port ${port}...`);
   });
